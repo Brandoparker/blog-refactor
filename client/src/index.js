d1235f5c80ce09266cc31b9696bae28601d645ca
Spencer Richards <spencer.richards7@gmail.com>
2019-03-22 16:11:03 -0600

    remove service worker

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter, } from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
@@ -11,5 +10,3 @@ ReactDOM.render(
   </BrowserRouter>, 
   document.getElementById('root')
 );
-
-serviceWorker.unregister();
